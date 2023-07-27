ea95e7e8d6d359da323091266f76405283e1e107
Parth Jadhav <hiparth2001@gmail.com>
2023-07-28 02:08:14 +0530

    Added github repo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <nav className="flex justify-between items-center w-full mb-10 pt-3">
       <img src={logo} alt="sumz_logo" className="w-28 object-contain"/>
 
-      <button type="button" onClick={()=> window.open('https://github.com/parth2181')} className="black_btn">GitHub</button>
+      <button type="button" onClick={()=> window.open('https://github.com/parth2181/OpenAI-Article-Summarizer')} className="black_btn">GitHub</button>
     </nav>
 
     <h1 className="head_text">Summarize Articles with <br className="max-md:hidden" />
